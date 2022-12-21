60af90feac1bda0df91bf77843ccdd3786bbc7d0
Adeshina Lasisi <91054814+integral1@users.noreply.github.com>
2022-12-21 01:45:28 +0100

    updated agricultural enums

--- src/main/java/com/andela/telent/assessment/automaticirrigationsystem/common/enums/AgriculturalCropTypeEnum.java
+++ src/main/java/com/andela/telent/assessment/automaticirrigationsystem/common/enums/AgriculturalCropTypeEnum.java
@@ -1,15 +1,15 @@
 package com.andela.telent.assessment.automaticirrigationsystem.common.enums;
 
 public enum AgriculturalCropTypeEnum {
-    MAIZE(1_000_000),
-    FORAGE_CROPS(100),
-    FIBER_CROPS(100),
-    OIL_CROPS(10000),
-    FIBRE_CROPS(100),
-    ORNAMENTAL_CROPS(1),
-    INDUSTRIAL_CROPS(1),
-    ANNUAL_CROPS(1),
-    BIENNIAL_CROPS(1);
+    MAIZE(1000),
+    FORAGE_CROPS(2000),
+    FIBER_CROPS(3000),
+    OIL_CROPS(4000),
+    FIBRE_CROPS(5000),
+    ORNAMENTAL_CROPS(6000),
+    INDUSTRIAL_CROPS(7000),
+    ANNUAL_CROPS(8000),
+    BIENNIAL_CROPS(9000);
 
     private final float amountOfWater;
 
